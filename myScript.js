eab05939820e6a501fc6e8c48459011c8dd0923c
NoobAndCoding <167626518+NoobAndCoding@users.noreply.github.com>
2024-06-02 17:05:05 +0300

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,6 +1,13 @@
 
         function myFunction() {
           document.getElementById ("demo").innerHTML = "Hello to you too!";
+          let x,y,z,u,o,p;
+                x = 5
+                y = 7
+                z = 19
+                u = 1111
+                o = 2222
+                p = x+y*z*u*o;
         }
 
         function myFunction2() {
