94dd381172f76032880964feedfb83fe0ceaf0b1
NoobAndCoding <167626518+NoobAndCoding@users.noreply.github.com>
2024-06-02 13:46:10 +0300

    Create myScript.js

---
+++ myScript.js
@@ -0,0 +1,8 @@
+
+        function myFunction() {
+          document.getElementById ("demo").innerHTML = "Hello to you too!";
+        }
+
+        function myFunction2() {
+          document.getElementById ("demo").innerHTML = "Hello Everybody";
+        }
